7418509044a99b3544eef4a5a57499cad943a0a5
Steven Loy <loynin@gmail.com>
2017-05-15 16:55:32 -0700

    Update tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,40 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+--
+-- Delete database tournament if exist
 
+DROP DATABASE IF EXISTS tournament;
 
+-- Create database name tournament
+CREATE DATABASE tournament;
+
+-- Connect to database
+\c tournament;
+
+-- Create table name player
+CREATE TABLE players(
+    id serial,
+    name text
+    );
+
+-- Create table name matchese
+CREATE TABLE matches(
+    id serial,
+    winner integer,
+    loser integer
+    );
+
+-- Create view to select how many win a player has
+create view view_wins as
+select players.id, count(matches.id) as wins
+from players left outer join matches
+    on players.id = matches.winner
+group by players.id;
+
+-- Create view to select how many plays a player has
+create view view_played as
+select players.id, count(matches.id) as played
+from players left outer join matches
+    on players.id = matches.winner or players.id = matches.loser
+group by players.id;
